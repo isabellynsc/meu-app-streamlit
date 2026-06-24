9c70843c4c4f70e58624d83e0d26e398df059096
isabellynsc <isabellycintia@hotmail.com>
2026-06-24 09:54:07 -0300

    Add files via upload

--- vidas.py
+++ vidas.py
@@ -5,7 +5,7 @@ from io import BytesIO
 # ✅ CONFIGURAÇÃO
 st.set_page_config(
     page_title="Comparador de Contratos",
-    page_icon="flor_icon.JPG",
+    page_icon="flor_icon.png",
     layout="centered"
 )
 
